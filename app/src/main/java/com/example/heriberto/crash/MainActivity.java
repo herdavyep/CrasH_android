6b02ebf63388aa51d3f919e35a8a0b594d47c957
Heriberto David <heriberto@MacBook-Air-de-Heriberto.local>
2017-04-05 16:39:01 -0500

    Vista crear producto terminada 80%

--- app/src/main/java/com/example/heriberto/crash/MainActivity.java
+++ app/src/main/java/com/example/heriberto/crash/MainActivity.java
@@ -14,7 +14,7 @@ public class MainActivity extends AppCompatActivity {
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-        setContentView(R.layout.activity_login);
+        setContentView(R.layout.activity_crear_producto);
         /*Toolbar toolbar = (Toolbar) findViewById(R.id.toolbar);
         setSupportActionBar(toolbar);
 
