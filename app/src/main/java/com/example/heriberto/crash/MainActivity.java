96d1228fa9bb349624be0a06fc931a1e13e2c272
Heriberto David <heriberto@MacBook-Air-de-Heriberto.local>
2017-04-11 17:52:29 -0500

    Recycler view no sirve para formulario quemado
    La solución es un scroll view

--- app/src/main/java/com/example/heriberto/crash/MainActivity.java
+++ app/src/main/java/com/example/heriberto/crash/MainActivity.java
@@ -4,17 +4,39 @@ import android.os.Bundle;
 import android.support.design.widget.FloatingActionButton;
 import android.support.design.widget.Snackbar;
 import android.support.v7.app.AppCompatActivity;
+import android.support.v7.widget.LinearLayoutManager;
+import android.support.v7.widget.RecyclerView;
 import android.support.v7.widget.Toolbar;
 import android.view.View;
 import android.view.Menu;
 import android.view.MenuItem;
+
+import com.example.heriberto.crash.Adaptadores.AdapterCrearProductos;
+import com.example.heriberto.crash.clases.Productos;
+
+import java.util.ArrayList;
 
 public class MainActivity extends AppCompatActivity {
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-        setContentView(R.layout.activity_login);
+        setContentView(R.layout.activity_crear_producto);
+
+        RecyclerView mRecyclerView = (RecyclerView) findViewById(R.id.recyclerCrearProductos);
+
+        mRecyclerView.setHasFixedSize(true);
+
+        LinearLayoutManager mLayoutManager = new LinearLayoutManager(this);
+        mRecyclerView.setLayoutManager(mLayoutManager);
+
+        ArrayList<Productos> mDataset = new ArrayList<>();
+
+        AdapterCrearProductos mAdapter = new AdapterCrearProductos(mDataset);
+        mRecyclerView.setAdapter(mAdapter);
+
+
+
         /*Toolbar toolbar = (Toolbar) findViewById(R.id.toolbar);
         setSupportActionBar(toolbar);
 
