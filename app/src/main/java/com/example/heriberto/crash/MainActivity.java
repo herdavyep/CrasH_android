f5267a628628eee79bd30969b51910313e484e60
Heriberto David <heriberto@MacBook-Air-de-Heriberto.local>
2017-04-29 22:45:59 -0500

    Paquete de firebase

--- app/src/main/java/com/example/heriberto/crash/MainActivity.java
+++ app/src/main/java/com/example/heriberto/crash/MainActivity.java
@@ -37,7 +37,6 @@ public class MainActivity extends AppCompatActivity {
             }
         });
 
-
         /*RecyclerView mRecyclerView = (RecyclerView) findViewById(R.id.recyclerCrearProductos);
 
         mRecyclerView.setHasFixedSize(true);
---
+++ app/src/main/java/com/example/heriberto/crash/firebaseReferencias/Referencias.java
@@ -0,0 +1,15 @@
+package com.example.heriberto.crash.firebaseReferencias;
+
+/**
+ * Created by heriberto on 29/04/17.
+ */
+
+public class Referencias {
+
+    final public static String ALMACENES_REFERENCIA = "almacenes";
+
+}
+
+
+
+
